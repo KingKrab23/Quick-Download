82a84bf6893975ec21fa1da44db18e5b1c0bac0c
KingKrab23 <47616086+KingKrab23@users.noreply.github.com>
2019-03-05 14:55:14 -0600

    Update to include more tracker support

--- Gazelle Quick Download.user.js
+++ Gazelle Quick Download.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         GGn Quick Download
+// @name         Gazelle Quick Download
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.0-alpha
 // @description  Copy a page worth of (non-sticky) GGn download links to your clipboard for easy pasting
 // @author       KingKrab23
 // @match        https://*/torrents.php*
@@ -9,7 +9,7 @@
 // @require      https://code.jquery.com/jquery-1.7.2.min.js
 // ==/UserScript==
 
-$(".linkbox").before('<button id="copy_dl_links_to_clipboard" style="margin-top:15px;">Copy DL links to clipboard</button>');
+$(".linkbox").first().before('<button id="copy_dl_links_to_clipboard" style="margin-top:15px;">Copy DL links to clipboard</button>');
 
 var $temp = $("<textarea class='hidden'>");
 $("body").append($temp);
@@ -24,14 +24,31 @@ $("#copy_dl_links_to_clipboard").click(function () {
 });
 
 function copy_dl_links_to_clipboard() {
+    var icon = false;
     //--- Note that contains() is CASE SENSITIVE.
-    var specLink = $("a:contains('DL')");
+    var specLink = $("a:contains('DL')"); // GGn
+
+    if (!specLink.length || specLink.length === 0) {
+        icon = true;
+        specLink = $(".icon_disk_none");
+    }
 
     if (specLink.length) {
         var text_to_copy = "";
-        $.each( specLink, function( key, value ) {
-           text_to_copy += value + "\n";
-        });
+        if (icon === true) {
+            $.each( specLink, function( key, value ) {
+                var path = $(this).parent().attr('href');
+
+                var getUrl = window.location;
+                var baseUrl = getUrl.protocol + "//" + getUrl.host + "/";
+
+                text_to_copy += baseUrl + path + "\n";
+            });
+        } else {
+            $.each( specLink, function( key, value ) {
+                text_to_copy += value + "\n";
+            });
+        }
 
         copyToClipboard(text_to_copy);
         alert(specLink.length + ' torrents have been added to your clipboard');
